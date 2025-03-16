3676875deefbffbad8eafd1fb2e9878287da11c0
MK2706 <mn2002manoj@gmail.com>
2025-03-16 16:03:10 +0530

    main.py

--- Interview_app/main.py
+++ Interview_app/main.py
@@ -11,7 +11,11 @@ try:
 except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pymongo"])
     import pymongo  # Import again after installation
-
+try:
+    import pymongo
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "bcrypt"])
+    import bcrypt  # Import again after installation
 
 
 import streamlit as st
